0d281af117abfd8463bc63988d1147e7f132f7de
bairog <44995181+bairog@users.noreply.github.com>
2021-10-19 06:45:38 +0300

    Add files via upload

---
+++ FileLoggerTestConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileLoggerTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Logs", "app.log");
+            var LogFileContent = ReadLogFile(filePath);
+
+            Console.WriteLine(LogFileContent);
+            Console.ReadKey();
+        }
+
+
+        internal static string ReadLogFile(string filePath)
+        {
+            try
+            {
+                using (FileStream stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (IOException ex)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return ex.ToString();
+            }
+        }
+    }
+}
